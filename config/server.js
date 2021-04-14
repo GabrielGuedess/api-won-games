5ca75ef3232612196c6443620fe427719429d5e4
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-04-14 10:35:27 -0300

    strapi custom admin home page

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a45a2d08a422fa91afe431fc9c61fdc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fae25aec-8526-438b-92ee-15cc85abacca'
+};
