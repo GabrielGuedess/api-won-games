e7814ea7a6dca2f37cab36b0af90a702cf0b25af
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-04-16 11:29:48 -0300

    ci: add push dockerhub

---
+++ config/env/production/database.js
@@ -0,0 +1,26 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL")
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+      },
+      options: {},
+    },
+  },
+});
